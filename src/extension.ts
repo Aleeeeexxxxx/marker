8dbaa712dcaaa82ba7980fae132105d3effc85c3
I548435 <alex.han@sap.com>
2024-05-21 01:51:52 +0000

    chore: centralize token manager

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,43 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { TokenManager } from "./token";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const tokenMngr = new TokenManager();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "marker" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "remarker.editor.menu.mark.highlight",
+      () => {
+        if (!vscode.window.activeTextEditor) {
+          return;
+        }
+        const { document, selection } = vscode.window.activeTextEditor;
+        tokenMngr.add(document.getText(selection));
+      }
+    ),
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('marker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Marker!');
-	});
+    vscode.commands.registerCommand("remarker.editor.menu.mark.remove", () => {
+      if (!vscode.window.activeTextEditor) {
+        return;
+      }
+      const { document, selection } = vscode.window.activeTextEditor;
+      tokenMngr.remove(document.getText(selection));
+    }),
 
-	context.subscriptions.push(disposable);
+    vscode.window.onDidChangeActiveTextEditor((editor) => {
+      if (editor) {
+        console.log("Active editor changed to: ", editor.document.fileName);
+      }
+    }),
+
+    vscode.workspace.onDidChangeTextDocument((event) => {
+      console.log(`event: ${JSON.stringify(event)}`);
+    })
+  );
 }
 
 // This method is called when your extension is deactivated
