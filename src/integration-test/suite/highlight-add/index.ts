aaa5c42a1e28f6fb1060f2efb05eaa6cca055c32
Alex <hanalex109@163.com>
2024-07-25 13:11:00 +0000

    chore: remove debug

--- src/integration-test/suite/highlight-add/index.ts
+++ src/integration-test/suite/highlight-add/index.ts
@@ -11,5 +11,5 @@ export const steps: IIntegrationTestStep[] = [
     new StepOpenTextDocument(path.join(__dirname, "./test.txt")),
     new StepSelectInTextDocument(3, 5),
     new StepExecuteCommand("marker.editor.menu.mark.highlight"),
-    new StepValidateHighlightItemIfExist("add"),
+    new StepValidateHighlightItemIfExist("dd"),
 ];
